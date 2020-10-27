9c6307dbe240c5d7e28c4a43ba78c14f12b2cee4
Edwin Mah <me@edwinmah.com>
2020-10-27 14:20:06 -0400

    feat: initial commit

---
+++ templates/template-social-link-optimizer.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Template Name: Social Link Optimizer (in child theme)
+ * Template Post Type: page
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+	?>
+
+			<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+			<?php
+		
+			get_template_part( 'template-parts/entry-header' );
+		
+			if ( ! is_search() ) {
+				get_template_part( 'template-parts/featured-image' );
+			}
+		
+			?>
+		
+			<div class="post-inner">
+		
+				<div class="entry-content gpa-social-link-optimizer">
+		
+					<?php
+					if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
+						the_excerpt();
+					} elseif (is_page('social-bio')) {
+					?>
+						<ul class="gpa-social-list">
+							<?php
+								$args = array(
+									'post_type' => 'social_link'
+								);
+								$the_query = new WP_Query( $args );
+								
+								if ( $the_query->have_posts() ) {
+									while ( $the_query->have_posts() ) {
+										$the_query->the_post();
+										echo '<li>';
+										echo '<h2 class="gpa-social-title">';
+										echo '<a href="' . esc_url( get_permalink() ) . '">';
+										the_title();
+										echo '</a>';
+										echo '</h2>';
+										echo '<div class="gpa-social-thumbnail">';
+										the_post_thumbnail();
+										echo '</div>';
+										echo '</li>';
+									}
+								} else {
+									echo '<p>No Social Bio Links</p>';
+								}
+								wp_reset_postdata();
+							?>
+						</ul>
+					<?php
+					} else {
+						the_content( __( 'Continue reading', 'twentytwenty' ) );
+					}
+					?>
+		
+				</div><!-- .entry-content -->
+		
+			</div><!-- .post-inner -->
+		
+			<div class="section-inner">
+				<?php
+				wp_link_pages(
+					array(
+						'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Page', 'twentytwenty' ) . '"><span class="label">' . __( 'Pages:', 'twentytwenty' ) . '</span>',
+						'after'       => '</nav>',
+						'link_before' => '<span class="page-number">',
+						'link_after'  => '</span>',
+					)
+				);
+		
+				edit_post_link();
+		
+				// Single bottom post meta.
+				twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+		
+				if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
+		
+					get_template_part( 'template-parts/entry-author-bio' );
+		
+				}
+				?>
+		
+			</div><!-- .section-inner -->
+		
+			<?php
+		
+			if ( is_single() ) {
+		
+				get_template_part( 'template-parts/navigation' );
+		
+			}
+		
+			/**
+			 *  Output comments wrapper if it's a post, or if comments are open,
+			 * or if there's a comment number – and check for password.
+			 * */
+			if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
+
+			?>
+		
+				<div class="comments-wrapper section-inner">
+		
+					<?php comments_template(); ?>
+		
+				</div><!-- .comments-wrapper -->
+		
+			<?php
+			}
+			?>
+		
+		</article><!-- .post -->
+
+	<?php
+		}
+	}
+	?>
+
+</main><!-- #site-content -->
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
