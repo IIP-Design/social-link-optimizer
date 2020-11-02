94bbafe8c759c39047ff9cc3171ba4c3c01177d0
Edwin Mah <me@edwinmah.com>
2020-11-02 09:22:27 -0500

    refactor: remove unneeded template parts

--- templates/template-social-link-optimizer.php
+++ templates/template-social-link-optimizer.php
@@ -22,13 +22,7 @@ get_header();
       <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
       <?php
-
       get_template_part( 'template-parts/entry-header' );
-
-      if ( ! is_search() ) {
-        get_template_part( 'template-parts/featured-image' );
-      }
-
       ?>
     
       <div class="post-inner">
@@ -135,40 +129,10 @@ get_header();
           )
         );
 
-        edit_post_link();
-
-        // Single bottom post meta.
-        twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
-
-        if ( post_type_supports( get_post_type( get_the_ID() ), 'author' ) && is_single() ) {
-
-          get_template_part( 'template-parts/entry-author-bio' );
-
-        }
+				edit_post_link();
         ?>
     
       </div><!-- .section-inner -->
-    
-      <?php
-
-      if ( is_single() ) {
-        get_template_part( 'template-parts/navigation' );
-      }
-
-      /**
-       *  Output comments wrapper if it's a post, or if comments are open,
-       * or if there's a comment number – and check for password.
-       * */
-      if ( ( is_single() || is_page() ) && ( comments_open() || get_comments_number() ) && ! post_password_required() ) {
-        ?>
-
-        <div class="comments-wrapper section-inner">
-          <?php comments_template(); ?>
-        </div><!-- .comments-wrapper -->
-
-        <?php
-      }
-      ?>
     
       </article><!-- .post -->
 
