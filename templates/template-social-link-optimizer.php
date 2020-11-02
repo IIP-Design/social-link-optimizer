68e6660d745c8703ac314d0984e21643cdc2a02d
Edwin Mah <me@edwinmah.com>
2020-11-02 10:22:24 -0500

    style: convert tabs to spaces

--- templates/template-social-link-optimizer.php
+++ templates/template-social-link-optimizer.php
@@ -29,85 +29,85 @@ get_header();
     
         <div class="entry-content gpa-social-link-optimizer">
     
-					<?php
-					$is_gpaslo_page_template = is_page_template( '/templates/template-social-link-optimizer.php' );
+          <?php
+          $is_gpaslo_page_template = is_page_template( '/templates/template-social-link-optimizer.php' );
 
           if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
             the_excerpt();
           } elseif ( $is_gpaslo_page_template ) {
-						$all_settings = get_option( 'gpalab_slo_settings_option_name' );
-						$display_as = $all_settings['display_gpalab_slo_as_a_0'];
-						$layout = ( isset( $display_as ) && $display_as !== '' )
-							? $display_as
-							: 'grid';
+            $all_settings = get_option( 'gpalab_slo_settings_option_name' );
+            $display_as = $all_settings['display_gpalab_slo_as_a_0'];
+            $layout = ( isset( $display_as ) && $display_as !== '' )
+              ? $display_as
+              : 'grid';
 
-						$social_accts = array(
-							'twitter'  => $all_settings['twitter_feed_3'],
-							'facebook' => $all_settings['facebook_page_1'],
-							'instagram' => $all_settings['instagram_feed_5'],
-							'youtube'  => $all_settings['youtube_channel_4'],
-							'linkedin' => $all_settings['linkedin_profile_2'],
-						);
+            $social_accts = array(
+              'twitter'  => $all_settings['twitter_feed_3'],
+              'facebook' => $all_settings['facebook_page_1'],
+              'instagram' => $all_settings['instagram_feed_5'],
+              'youtube'  => $all_settings['youtube_channel_4'],
+              'linkedin' => $all_settings['linkedin_profile_2'],
+            );
 
-						$assets_dir = plugins_url( 'social-link-optimizer' ) . '/assets/';
-					?>
+            $assets_dir = plugins_url( 'social-link-optimizer' ) . '/assets/';
+          ?>
 
-						<aside>
-							<h3 id="social-accts" class="hide-visually">
-								social media accounts
-							</h3>
-							<ul class="social-media-accts" aria-describedby="social-accts">
-							<?php
-								foreach ( $social_accts as $key => $value ) {
-									if ( isset( $value ) && $value !== '' ) {
-										echo '<li>';
-										echo '<a href="' . $value . '">';
-										echo '<img src="' . $assets_dir . $key . '.svg" alt="" height="24" width="24">';
-										echo '<span class="hide-visually">' . $key . '</span>';
-										echo '</a>';
-										echo '</li>';
-									}
-								}
-							?>
-							</ul>
-						</aside>
+            <aside>
+              <h3 id="social-accts" class="hide-visually">
+                social media accounts
+              </h3>
+              <ul class="social-media-accts" aria-describedby="social-accts">
+              <?php
+                foreach ( $social_accts as $key => $value ) {
+                  if ( isset( $value ) && $value !== '' ) {
+                    echo '<li>';
+                    echo '<a href="' . $value . '">';
+                    echo '<img src="' . $assets_dir . $key . '.svg" alt="" height="24" width="24">';
+                    echo '<span class="hide-visually">' . $key . '</span>';
+                    echo '</a>';
+                    echo '</li>';
+                  }
+                }
+              ?>
+              </ul>
+            </aside>
 
-						<h2 id="instagram-posts" class="hide-visually">
-							Instagram posts
-						</h2>
-						<ul class="gpa-social-list <?php echo $layout; ?>" aria-describedby="instagram-posts">
-							<?php
+            <h2 id="instagram-posts" class="hide-visually">
+              Instagram posts
+            </h2>
+            <ul class="gpa-social-list <?php echo $layout; ?>" aria-describedby="instagram-posts">
+              <?php
 
-							$args      = array( 'post_type' => 'social_link' );
-							$the_query = new WP_Query( $args );
+              $args      = array( 'post_type' => 'social_link' );
+              $the_query = new WP_Query( $args );
 
-							if ( $the_query->have_posts() ) {
-								while ( $the_query->have_posts() ) {
-									$the_query->the_post();
-									$anchor_tag_open = '<a href="' . esc_url( get_permalink() ) . '">';
-									$anchor_tag_close = '</a>';
-							
-									echo '<li>';
-									echo '<article>';
-									echo '<h3 class="gpa-social-title">';
-									echo $layout == 'list' ? $anchor_tag_open : null;
-									the_title();
-									echo $layout == 'list' ? $anchor_tag_close : null;
-									echo '</h3>';
-									echo $layout == 'grid' ? $anchor_tag_open : null;
-									$layout == 'grid' ? the_post_thumbnail( 'post-thumbnail', ['class' => 'gpa-social-thumbnail'] ) : null;
-									echo $layout == 'grid' ? $anchor_tag_close : null;
-									echo '</article>';
-									echo '</li>';
-								}
-							} else {
-								echo '<p>No Social Bio Links</p>';
-							}
+              if ( $the_query->have_posts() ) {
+                while ( $the_query->have_posts() ) {
+                  $the_query->the_post();
+                  $anchor_tag_open = '<a href="' . esc_url( get_permalink() ) . '">';
+                  $anchor_tag_close = '</a>';
+              
+                  echo '<li>';
+                  echo '<article>';
+                  echo '<h3 class="gpa-social-title">';
+                  echo $layout == 'list' ? $anchor_tag_open : null;
+                  the_title();
+                  echo $layout == 'list' ? $anchor_tag_close : null;
+                  echo '</h3>';
+                  echo $layout == 'grid' ? $anchor_tag_open : null;
+                  $layout == 'grid' ? the_post_thumbnail( 'post-thumbnail', ['class' => 'gpa-social-thumbnail'] ) : null;
+                  echo $layout == 'grid' ? $anchor_tag_close : null;
+                  echo '</article>';
+                  echo '</li>';
+                }
+              } else {
+                echo '<p>No Social Bio Links</p>';
+              }
 
-							wp_reset_postdata();
+              wp_reset_postdata();
 
-							?>
-						</ul>
+              ?>
+            </ul>
             <?php
           } else {
             the_content( __( 'Continue reading', 'gpalab-slo' ) );
@@ -129,7 +129,7 @@ get_header();
           )
         );
 
-				edit_post_link();
+        edit_post_link();
         ?>
     
       </div><!-- .section-inner -->
