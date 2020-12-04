290dad04b786e060e5fed341a64819c4dc75c243
mrewers <mrewers@users.noreply.github.com>
2020-12-04 12:02:02 -0500

    fix: remove social link archive page

--- admin/class-cpt.php
+++ admin/class-cpt.php
@@ -109,7 +109,7 @@ class CPT {
       'show_in_admin_bar'   => true,
       'show_in_nav_menus'   => true,
       'can_export'          => true,
-      'has_archive'         => true,
+      'has_archive'         => false,
       'exclude_from_search' => false,
       'publicly_queryable'  => true,
       'rewrite'             => $rewrite,
