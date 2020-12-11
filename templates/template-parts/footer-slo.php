bf3a19205a4c88e91a9a7436af0d062abc2e25d8
Edwin Mah <me@edwinmah.com>
2020-12-11 07:24:28 -0500

    refactor: adjust escaping function

--- templates/template-parts/footer-slo.php
+++ templates/template-parts/footer-slo.php
@@ -11,7 +11,7 @@
       <a href="#gpalab-slo-site-header" class="back-to-top" >
         <?php
         /* translators: %s: HTML character for up arrow. */
-        printf( esc_html( __( 'Back to top %s', 'gpalab-slo' ) ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+        printf( esc_html__( 'Back to top %s', 'gpalab-slo' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
         ?>
       </a><!-- .back-to-top -->
     </footer>
