3f95934eb8fe2760e264eae678e3d0cdf4616c45
Edwin Mah <me@edwinmah.com>
2020-12-15 16:24:57 -0500

    feat: use up arrow icon for back to top

--- templates/template-parts/footer-slo.php
+++ templates/template-parts/footer-slo.php
@@ -5,13 +5,17 @@
  * @package GPALAB_SLO
  */
 
+ // Get the path the the plugin's assets.
+$assets_dir = GPALAB_SLO_URL . 'public/assets/';
 ?>
 
     <footer id="gpalab-slo-site-footer" role="contentinfo">
       <a href="#gpalab-slo-site-header" class="back-to-top" >
         <?php
-        /* translators: %s: HTML character for up arrow. */
-        printf( esc_html__( 'Back to top %s', 'gpalab-slo' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+        $up_icon  = '<img src=' . esc_attr( $assets_dir . 'arrow-up.svg' ) . ' alt="" height="50" width="50" class="back-to-top">';
+        $up_icon .= '<span class="hide-visually">' . __( 'back to top', 'gpalab-slo' ) . '</span>';
+
+        echo wp_kses( $up_icon, 'post' );
         ?>
       </a><!-- .back-to-top -->
     </footer>
