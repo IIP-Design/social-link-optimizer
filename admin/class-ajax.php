3b79096c7c32d6fb0af236a12140812b8b53e9a7
mrewers <mrewers@users.noreply.github.com>
2020-12-22 15:02:29 -0500

    feat: automatically generate SLO page when a new mission is added

--- admin/class-ajax.php
+++ admin/class-ajax.php
@@ -29,27 +29,61 @@ class Ajax {
     $this->slo_verify_nonce( $_POST['security'] );
     // phpcs:enable
 
-    $slo_settings = get_option( 'gpalab-slo-settings' );
-    $missions     = ! empty( $slo_settings ) ? $slo_settings : array();
+    // Generate a unique ID for the new mission.
+    $mission_id = wp_generate_uuid4();
+
+    // Create a corresponding SLO template page for the new mission.
+    $slo_page_id = $this->generate_slo_page( $mission_id );
 
     // Initialize an empty array to populate with placeholder values.
     $new_mission = array();
 
-    $new_mission['id']        = wp_generate_uuid4();
+    $new_mission['id']        = $mission_id;
+    $new_mission['page']      = $slo_page_id;
     $new_mission['title']     = 'New';
     $new_mission['type']      = 'grid';
     $new_mission['facebook']  = '';
+    $new_mission['flickr']    = '';
     $new_mission['instagram'] = '';
     $new_mission['linkedin']  = '';
     $new_mission['twitter']   = '';
-    $new_mission['youtube']   = '';
-    $new_mission['flickr']    = '';
     $new_mission['whatsapp']  = '';
+    $new_mission['youtube']   = '';
 
     // Add new mission into array of existing missions.
+    $slo_settings = get_option( 'gpalab-slo-settings' );
+    $missions     = ! empty( $slo_settings ) ? $slo_settings : array();
+
     array_push( $missions, $new_mission );
 
     update_option( 'gpalab-slo-settings', $missions );
+  }
+
+  /**
+   * Creates a new SLO page and associates it with the provided mission id.
+   *
+   * @param string $mission_id   The id of the mission the new page should be associated with.
+   * @return int                 The id of the newly created page.
+   *
+   * @since 0.0.1
+   */
+  private function generate_slo_page( $mission_id ) {
+    $slo_page_id = wp_insert_post(
+      array(
+        'ID'             => 0,
+        'comment_status' => 'closed',
+        'meta_input'     => array(
+          '_gpalab_slo_mission_select' => $mission_id,
+          '_wp_page_template'          => 'archive-gpalab-social-link.php',
+        ),
+        'ping_status'    => 'closed',
+        'post_status'    => 'publish',
+        'post_title'     => 'SLO ' . $mission_id,
+        'post_type'      => 'page',
+      )
+    );
+
+    return $slo_page_id;
   }
 
   /**
@@ -65,7 +99,7 @@ class Ajax {
     // phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotValidated
     $this->slo_verify_nonce( $_POST['security'] );
 
-    $id;
+    $id = null;
 
     if ( isset( $_POST['mission_id'] ) ) {
       $id = sanitize_text_field( wp_unslash( $_POST['mission_id'] ) );
@@ -79,12 +113,14 @@ class Ajax {
     }
 
     // Initialize the index of the selected mission.
-    $index;
+    $index   = null;
+    $page_id = 0;
 
-    // Retrieve the index value of the selected mission.
+    // Retrieve the index value of the selected mission and it's associated page id.
     foreach ( $slo_settings as $key => $setting ) {
       if ( $setting['id'] === $id ) {
-        $index = $key;
+        $index   = $key;
+        $page_id = ! empty( $setting['page'] ) ? $setting['page'] : 0;
         break;
       }
     }
@@ -96,6 +132,7 @@ class Ajax {
     $reindexed = array_values( $slo_settings );
 
     update_option( 'gpalab-slo-settings', $reindexed );
+    wp_delete_post( $page_id, true );
   }
 
   /**
@@ -106,7 +143,7 @@ class Ajax {
    * @since 0.0.1
    */
   private function slo_verify_nonce( $security ) {
-    $nonce;
+    $nonce = null;
 
     // Make sure nonce is set.
     if ( ! isset( $security ) ) {
