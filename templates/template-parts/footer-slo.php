3b786f6e78cd7bfaa8baae75273ddf6af2cedf5f
Edwin Mah <me@edwinmah.com>
2021-01-22 08:42:18 -0500

    refactor: update back to top styling

--- templates/template-parts/footer-slo.php
+++ templates/template-parts/footer-slo.php
@@ -13,8 +13,8 @@ $assets_dir = GPALAB_SLO_URL . 'public/assets/';
     <footer id="gpalab-slo-site-footer" role="contentinfo">
       <a href="#gpalab-slo" class="back-to-top" role="navigation" aria-label="back to top">
         <?php
-        $up_icon  = '<img src=' . esc_attr( $assets_dir . 'arrow-up.svg' ) . ' alt="" height="50" width="50" class="back-to-top">';
-        $up_icon .= '<span class="hide-visually">' . __( 'back to top', 'gpalab-slo' ) . '</span>';
+        $up_icon  = '<span>' . __( 'back to top', 'gpalab-slo' ) . '</span>';
+        $up_icon .= '<img src=' . esc_attr( $assets_dir . 'arrow-up.svg' ) . ' alt="" height="50" width="50" class="back-to-top">';
 
         echo wp_kses( $up_icon, 'post' );
         ?>
